d6d2a490137bdf03787fcb4f4ff847109eb6d99c
SAISH SAWANT <saish1233@gmail.com>
2018-09-04 16:23:31 +0530

    Initial commit

---
+++ app/src/main/java/com/sawant/capture_signature/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sawant.capture_signature;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new SignatureMainLayout(this));
+
+
+    }
+}
